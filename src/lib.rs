8fe52fc1cd41e2471bb8f992118d66bd615c181e
Stefan Rakel <rakel@embedded.rwth-aachen.de>
2019-07-02 16:06:37 +0200

    Reworked refresh function

--- src/lib.rs
+++ src/lib.rs
@@ -8,8 +8,8 @@
 
 use embedded_hal as hal;
 
-const WIDTH: u8 = 44;
-const HEIGHT: u8 = 68;
+const WIDTH: u8 = 144;
+const HEIGHT: u8 = 168;
 const BUFFER_SIZE: usize = WIDTH as usize * HEIGHT as usize / 8; //this works because 144*168 is dividable by 8 if the dimensions ever change make sure to handle this!
 
 /// The Sharp Memory Display driver
@@ -37,7 +37,7 @@ where
             buffer: [0; BUFFER_SIZE],
             vcom: true,
         };
-        let result = display.cs.set_high();
+        let result = display.cs.set_low();
         if result.is_err() {
             return Err(());
         }
@@ -51,7 +51,7 @@ where
             *val = 0;
         }
         //Chip select
-        if let Err(_) = self.cs.set_low() {
+        if let Err(_) = self.cs.set_high() {
             return Err(());
         };
         let command = self.command(CommandBit::ClearBit);
@@ -63,7 +63,7 @@ where
         } else {
             failure = true;
         }
-        if let Err(_) = self.cs.set_high() {
+        if let Err(_) = self.cs.set_low() {
             return Err(());
         };
         if failure {
@@ -74,49 +74,25 @@ where
 
     /// Refresh function. Should be called periodically with >1Hz to update display
     pub fn refresh(&mut self) -> Result<(), ()> {
+        const SIZE: usize = BUFFER_SIZE + 1 + HEIGHT as usize * 2 + 1; //1 byte command, heigh * 2 byte per line (number, data, end), 1 byte end
+        let mut buffer: [u8; SIZE] = [0; SIZE];
+        buffer[0] = self.command(CommandBit::WriteCmd);
+        const BYTES_PER_LINE: u8 = WIDTH / 8 + 2;
+        for i in 0..HEIGHT {
+            let buffer_index: usize = (i * BYTES_PER_LINE + 1) as usize;
+            buffer[buffer_index] = i + 1;
+            let slice_index: usize = (i * WIDTH / 8) as usize;
+            let slice = &self.buffer[slice_index..slice_index + WIDTH as usize / 8];
+            buffer[buffer_index + 1 .. buffer_index + WIDTH as usize / 8  - 1].copy_from_slice(&slice);
+            buffer[buffer_index + WIDTH as usize / 8] = 0;
+        }
         
         //Chipselect
-        if let Err(_) = self.cs.set_low() {
-            return Err(());
-        };
+        //TODO: better error handling
+        let _ = self.cs.set_high(); 
+        let _ = self.com.write(&buffer);    
+        let _ = self.cs.set_low();
         
-        let cmd = self.command(CommandBit::WriteCmd);
-        let mut failure = false;
-        if let Ok(_) = self.write_byte(cmd) {
-            let mut old_line: u8 = 1;
-            let mut current_line: u8;
-            for i in 0..BUFFER_SIZE {
-                if let Err(_) = self.write_byte(self.buffer[i]) {
-                    failure = true;
-                    break;
-                }
-                current_line = (((i+1)/(WIDTH as usize/8)) + 1) as u8;
-                if current_line != old_line {
-                    if let Err(_) = self.write_byte(0) {
-                        failure = true;
-                        break;
-                    }
-                    if current_line <= HEIGHT {
-                        if let Err(_) = self.write_byte(current_line as u8 ) {
-                            failure = true;
-                            break;
-                        }
-                    }
-                    old_line = current_line;
-                }
-            }
-
-            if let Err(_) = self.write_byte(0) {
-                failure = true;
-            }
-        }
-        if let Err(_) = self.cs.set_high() {
-            return Err(());
-        };
-
-        if failure {
-            return Err(());
-        }
         Ok(())
     }
 
@@ -147,7 +123,7 @@ where
     fn command(&mut self, command: CommandBit) -> u8 {
         let mut command = command as u8;
         if self.vcom {
-            command |= 0x40; //Magic number from datasheet
+            command |= 0x02;
         }
         self.toggle_vcom();
         command
@@ -167,14 +143,24 @@ where
     }
 }
 
+impl<E> From<E> for ()
+    where
+        E: hal::digital::v2::OutputPin::Error
+{
+    fn from(err: E) -> Self {
+        ()
+    }
+
+}
+
 enum CommandBit {
-    WriteCmd = 0x80,
-    ClearBit = 0x20,
+    WriteCmd = 0x01,
+    ClearBit = 0x04,
 }
 
 fn get_index(x: u8, y: u8) -> (usize, u8) {
-    let into = y * WIDTH + x;
+    let into = y as usize * WIDTH as usize + x as usize;
     let index = into / 8;
     let bit = into % 8;
-    (index as usize, bit)
+    (index, bit as u8)
 }
